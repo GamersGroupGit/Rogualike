209caaa29d9a3d660c0dcc8a3a88bb0993443238
SUPERGAMESGROUP <SUPERGAMESGROUP@protonmail.com>
2019-06-09 09:09:08 +0300

    + dash

--- LastDungeon/Assets/Scripts/PlayerController.cs
+++ LastDungeon/Assets/Scripts/PlayerController.cs
@@ -9,10 +9,17 @@ public class PlayerController : MonoBehaviour {
     private Vector2 moveVelocity;
     public Animator animator;
 
+    public float dashSpeed;
+    private float dashTime;
+    public float startDashTime;
+    private int direction;
+
+
     private void Start()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        dashTime = startDashTime;
     }
 
     private void Update()
@@ -37,6 +44,55 @@ public class PlayerController : MonoBehaviour {
         } else if (Input.GetKeyUp(KeyCode.F))
         {
             animator.SetBool("Strike", false);
+        }
+
+        if (direction == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                direction = 1;
+            }
+            else if (Input.GetKeyDown(KeyCode.L))
+            {
+                direction = 2;
+            }
+            else if (Input.GetKeyDown(KeyCode.I))
+            {
+                direction = 3;
+            }
+            else if (Input.GetKeyDown(KeyCode.K))
+            {
+                direction = 4;
+            }
+        }
+        else
+        {
+            if (dashTime <= 0)
+            {
+                direction = 0;
+                dashTime = startDashTime;
+                rb.velocity = Vector2.zero;
+            }
+            else
+            {
+                dashTime -= Time.deltaTime;
+                if (direction == 1)
+                {
+                    rb.AddForce(Vector2.left * dashSpeed);
+                }
+                else if (direction == 2)
+                {
+                    rb.AddForce(Vector2.right * dashSpeed);
+                }
+                else if (direction == 3)
+                {
+                    rb.AddForce(Vector2.up * dashSpeed);
+                }
+                else if (direction == 4)
+                {
+                    rb.AddForce(Vector2.down * dashSpeed);
+                }
+            }
         }
     }
 
